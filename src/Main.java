dc43503dc7d2b5fa2ceac5345e206fcd4b2f87b1
GoldenEagle75 <marshall.muhlhauser@gmail.com>
2023-06-03 12:27:42 -0400

    Finished first part

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,41 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int intOperandA = 1;
+        int intOperandB = 5;
+        int intSum = 23;
+        int intProduct = 17;
+        int intDifference = 11;
+        int intQuotient = 54;
+        int intModulo = 4;
+        double doubleOperandA = 1.5;
+        double doubleOperandB = 5.2;
+        double doubleSum = 23.3;
+        double doubleProduct = 17.2;
+        double doubleDifference = 11.3;
+        double doubleQuotient = 54.3;
+        double doubleModulo = 4.4; //added in double doubleModulo even though it was not specifically requested
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is " + intSum + ".");
+        intProduct = intOperandA * intOperandB;
+        System.out.println("The product of " + intOperandA + " and " + intOperandB + " is " + intProduct + ".");
+        intDifference = intOperandA - intOperandB;
+        System.out.println("The difference between " + intOperandA + " and " + intOperandB + " is " + intDifference + ".");
+        intQuotient = intOperandA / intOperandB;
+        System.out.println("The quotient of " + intOperandA + " and " + intOperandB + " is " + intQuotient + ".");
+        intModulo = intOperandA % intOperandB;
+        System.out.println("The modulo of " + intOperandA + " and " + intOperandB + " is " + intModulo + ".");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        doubleSum = doubleOperandA + doubleOperandB;
+        System.out.println("The sum of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum + "." );
+        doubleProduct = doubleOperandA * doubleOperandB;
+        System.out.println("The product of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct + "." );
+        doubleDifference = doubleOperandA - doubleOperandB;
+        System.out.println("The difference between " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference + "." );
+        doubleQuotient = doubleOperandA / doubleOperandB;
+        System.out.println("The quotient of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleQuotient + "." );
+        doubleModulo = doubleOperandA % doubleOperandB;
+        System.out.println("The modulo of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleModulo + "." );
+
     }
 }
